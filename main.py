e503780b7270748da6a1a38455a257c185fba8ef
Sorna Raja Prabhu <105154789+SornaRajaPrabhu@users.noreply.github.com>
2023-05-03 11:36:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask
+import requests
+import json
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def mainWork():
-    return "Hello World"
+    username = "prabhu@relanto.ai"
+    password = "Anaplan@April2023"
+    
+    auth_url = 'https://auth.anaplan.com/token/authenticate'
+    auth_json = requests.post(
+        url=auth_url,
+        auth=(username, password)
+    ).json()
+    if auth_json['status'] == 'SUCCESS':
+        authToken = 'AnaplanAuthToken ' + auth_json['tokenInfo']['tokenValue']
+        return "AnaplanAuthToken" + auth_json['status']
 
 
 if __name__ == '__main__':
